82f6e6fe017afdaa550d63ee5a5ca6b0be4b75ce
renee-coder <71918527+renee-coder@users.noreply.github.com>
2020-10-31 11:43:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -20,3 +20,19 @@ classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/K
 function modelLoaded() {
     console.log('Model Loaded!');
 }
+
+function check(){
+    img=document.getElementById('captured_image');
+    classifier.classify(img,gotResult);
+}
+
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }    
+    else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML=results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3); 
+    }
+}
